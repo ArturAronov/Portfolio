4f38faf93473652a194475b94ac08eab1f1c574d
arturaronov <unionco@icloud.com>
2021-07-20 01:53:47 +0800

    finalized color schemes, finished video menu animations

--- src/components/vid2.js
+++ src/components/vid2.js
@@ -2,8 +2,15 @@ import Video from '../vids/2.mp4'
 
 export const Vid2=function(){
   return(
-    <div className='video'>
-      <video autoPlay muted loop src={Video} type='video/mp4'/>
+    <div className='vidContainer'>
+      <video 
+        id='v2'
+        className='video'
+        autoPlay 
+        muted 
+        loop src={Video} 
+        type='video/mp4'
+      />
     </div>
   );
 };
--- src/components/vid3.js
+++ src/components/vid3.js
@@ -2,8 +2,15 @@ import Video from '../vids/3.mp4'
 
 export const Vid3=function(){
   return(
-    <div className='video'>
-      <video autoPlay muted loop src={Video} type='video/mp4'/>
+    <div  className='vidContainer'>
+      <video 
+        id='v3'
+        className='video'
+        autoPlay 
+        muted 
+        loop src={Video} 
+        type='video/mp4'
+      />
     </div>
   );
 };
