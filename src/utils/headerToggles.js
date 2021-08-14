3160e279eba1c8a8af5b3b0dbe0293d9f5f4e601
arturaronov <unionco@icloud.com>
2021-08-14 19:56:51 +0800

    starting again from scratch

--- src/utils/headerToggles.js
+++
@@ -1,7 +0,0 @@
-export const toggleColors=function(state, changeState, light, dark){
-  state===dark?changeState(light):changeState(dark);
-};
-
-export const toggleIcons=function(state, changeState, flex, none){
-  state===flex?changeState(none):changeState(flex);
-};
--- src/utils/test.js
+++
@@ -1,9 +0,0 @@
-export let lol= 'none'
-
-export const test=function(){
-  if(lol==='none'){
-    lol='flex'
-  }else{
-    lol='none'
-  }
-}
